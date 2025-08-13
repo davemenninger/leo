abe15e42fa6b5b360ec466671d9ef841aa25876a
Dave Menninger <dave.menninger@gmail.com>
2025-08-13 19:15:45 -0400

    fix fat arrow

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -5,7 +5,7 @@ const urlsToCache = [
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("pwa-assets")
-    .then(cache -> {
+    .then(cache => {
       return cache.addAll(urlsToCache);
     });
   );
