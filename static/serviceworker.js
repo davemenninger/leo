a8b727827a7f3067ee7f025b0ae59da81541fdbf
Dave Menninger <dave.menninger@gmail.com>
2025-08-14 07:38:14 -0400

    version the sw cache

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -1,10 +1,12 @@
+const version = "2";
+
 const urlsToCache = [
   "leo.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("pwa-assets")
+    caches.open("pwa-assets" + version)
     .then(cache => {
       return cache.addAll(urlsToCache);
     });
